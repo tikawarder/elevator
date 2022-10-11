02dbeee36991d70b1bc0c7f6ab724238f4c2a488
Tamas Biro <tamas_biro@epam.com>
2022-10-11 15:49:19 +0200

    Remove @SpringBootTest

--- src/test/java/com/epam/elavator/ElavatorApplicationTests.java
+++ src/test/java/com/epam/elavator/ElavatorApplicationTests.java
@@ -3,7 +3,6 @@ package com.epam.elavator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ElavatorApplicationTests {
 
 	@Test
